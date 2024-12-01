c4593b9caa124b528fdac751fbeeaf1f414a202e
Ulf Haueisen <ulf@dvlp.de>
2024-12-01 10:30:02 +0100

    Day 1: Historian Hysteria

---
+++ 01 Historian Hysteria/Part1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        List<int> l1 = [];
+        List<int> l2 = [];
+
+        var sr = new StreamReader(@"..\..\input.txt");
+        int result = 0;
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            var a = line.Split("   ");
+            l1.Add(int.Parse(a[0]));
+            l2.Add(int.Parse(a[1]));
+        }
+        l1.Sort();
+        l2.Sort();
+        for (int i = 0; i < l1.Count; i++)
+        {
+            var d = (Math.Abs(l1[i] - l2[i]));
+            result += d;
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ 01 Historian Hysteria/Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        List<int> left = [];
+        Dictionary<int, int> right = [];
+        var sr = new StreamReader(@"..\..\input.txt");
+        int result = 0;
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            var a = line.Split("   ");
+            left.Add(int.Parse(a[0]));
+            int rv = int.Parse(a[1]);
+            if (right.TryGetValue(rv, out int count))
+            {
+                right[rv] = count + 1;
+            }
+            else
+            {
+                right[rv] = 1;
+            }
+        }
+        left.Sort();
+        for (int i = 0; i < left.Count; i++)
+        {
+            if (right.TryGetValue(left[i], out int count))
+            {
+                result += count * left[i];
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
