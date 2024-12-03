015c714954f5f751af2de6d2b44c6ffbd11d3281
Ulf Haueisen <ulf@dvlp.de>
2024-12-03 12:39:36 +0100

    Day 3: Mull It Over

---
+++ 03 Mull It Over/Part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        long result = 0;
+        var data = System.IO.File.ReadAllText(@"..\..\input.txt");
+        var regex = new Regex(@"mul\((\d+),(\d+)\)");
+        foreach (Match match in regex.Matches(data))
+        {
+            var a = long.Parse(match.Groups[1].Value);
+            var b = long.Parse(match.Groups[2].Value);
+            result += a * b;
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ 03 Mull It Over/Part2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        long result = 0;
+        var data = System.IO.File.ReadAllText(@"..\..\input.txt");
+        var regex = new Regex(@"(mul)\((\d+),(\d+)\)|(do)\(\)|(don't)\(\)");
+        bool enabled = true;
+        foreach (Match match in regex.Matches(data))
+        {
+            if (match.Groups[1].Value == "mul")
+            {
+                if (enabled)
+                {
+                    var a = long.Parse(match.Groups[2].Value);
+                    var b = long.Parse(match.Groups[3].Value);
+                    result += a * b;
+                }
+            }
+            else if (match.Groups[4].Value == "do")
+            {
+                enabled = true;
+            }
+            else if (match.Groups[5].Value == "don't")
+            {
+                enabled = false;
+            }
+            else
+            {
+                throw new System.Exception("Invalid match");
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
