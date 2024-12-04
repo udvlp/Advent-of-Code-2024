625ba7fb6ba0298cee44150434c7378431a0c2f0
Ulf Haueisen <ulf@dvlp.de>
2024-12-04 07:14:08 +0100

    Day 4: Ceres Search

---
+++ 04 Ceres Search/Part1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int result = 0;
+        var field = File.ReadAllLines(@"..\..\input.txt");
+        int w = field[0].Length;
+        int h = field.Length;
+        string s = "XMAS";
+        for (int n = 0; n < 9; n++)
+        {
+            int dx = n % 3 - 1;
+            int dy = n / 3 - 1;
+            if (dx == 0 && dy == 0) continue;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    bool found = true;
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        int xx = x + i * dx;
+                        int yy = y + i * dy;
+                        if (xx < 0 || xx >= w || yy < 0 || yy >= h || field[yy][xx] != s[i])
+                        {
+                            found = false;
+                            break;
+                        }
+                    }
+                    if (found) result++;
+                }
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ 04 Ceres Search/Part2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int result = 0;
+        var field = File.ReadAllLines(@"..\..\input.txt");
+        int w = field[0].Length;
+        int h = field.Length;
+        for (int y = 1; y < h - 1; y++)
+        {
+            for (int x = 1; x < w - 1; x++)
+            {
+                if (field[y][x] == 'A')
+                {
+                    if (
+                        (
+                            field[y - 1][x - 1] == 'M' && field[y + 1][x + 1] == 'S' ||
+                            field[y - 1][x - 1] == 'S' && field[y + 1][x + 1] == 'M'
+                        ) && (
+                            field[y - 1][x + 1] == 'M' && field[y + 1][x - 1] == 'S' ||
+                            field[y - 1][x + 1] == 'S' && field[y + 1][x - 1] == 'M'
+                        )
+                       )
+                    {
+                        result++;
+                    }
+
+                }
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
