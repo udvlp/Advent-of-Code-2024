95043b366c711e72ec86dec08c22b52a302d47ba
Ulf Haueisen <ulf@dvlp.de>
2024-12-07 14:10:31 +0100

    Day 7: Bridge Repair

---
+++ 07 Bridge Repair/Part1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AoC;
+
+internal class Program
+{
+	static void Main()
+	{
+		var sr = new StreamReader(@"..\..\input.txt");
+		ulong result = 0;
+		while (!sr.EndOfStream)
+		{
+			var line = sr.ReadLine();
+			var parts = Array.ConvertAll(line.Split([' ', ':'], StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
+
+			bool Evaluate(int i, ulong v)
+			{
+				if (i >= parts.Length)
+				{
+					return (v == parts[0]);
+				}
+				else
+				{
+					return
+						Evaluate(i + 1, parts[i] + v) ||
+						Evaluate(i + 1, parts[i] * v);
+				}
+			}
+
+			if (Evaluate(2, parts[1])) result += parts[0];
+		}
+		Console.WriteLine(result);
+	}
+}
---
+++ 07 Bridge Repair/Part2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace AoC;
+
+internal class Program
+{
+	static ulong ConcatNumbers(ulong a, ulong b)
+	{
+		ulong r = 0;
+		ulong s = 1;
+		while (b > 0)
+		{
+			r += s * (b % 10);
+			s *= 10;
+			b /= 10;
+		}
+		while (a > 0)
+		{
+			r += s * (a % 10);
+			s *= 10;
+			a /= 10;
+		}
+		return r;
+	}
+
+	static void Main()
+	{
+		var sr = new StreamReader(@"..\..\input.txt");
+		ulong result = 0;
+		while (!sr.EndOfStream)
+		{
+			var line = sr.ReadLine();
+			var parts = Array.ConvertAll(line.Split([' ', ':'], StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
+
+			bool Evaluate(int i, ulong v)
+			{
+				if (i >= parts.Length)
+				{
+					return v == parts[0];
+				}
+				else
+				{
+					return
+						Evaluate(i + 1, v + parts[i]) ||
+						Evaluate(i + 1, v * parts[i]) ||
+						Evaluate(i + 1, ConcatNumbers(v, parts[i]));
+				}
+			}
+			if (Evaluate(2, parts[1])) result += parts[0];
+		}
+		Console.WriteLine(result);
+	}
+}
