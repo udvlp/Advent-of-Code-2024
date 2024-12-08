a8c9fae396958b7e92fe75945763e6f1beef9c14
Ulf Haueisen <ulf@dvlp.de>
2024-12-08 09:44:51 +0100

    Day 8: Resonant Collinearity

---
+++ 08 Resonant Collinearity/Part1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace AoC;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var lines = File.ReadAllLines(@"..\..\input.txt");
+		int width = lines[0].Length;
+		int height = lines.Count();
+
+		Dictionary<char, List<(int x, int y)>> map = new();
+		HashSet<(int x, int y)> locations = new();
+
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				if (lines[y][x] != '.')
+				{
+					if (map.TryGetValue(lines[y][x], out var list))
+					{
+						list.Add((x, y));
+					}
+					else
+					{
+						map[lines[y][x]] = new() { (x, y) };
+					}
+				}
+			}
+		}
+
+		foreach (var item in map)
+		{
+			var l = item.Value;
+			for (int i = 0; i < l.Count; i++)
+			{
+				for (int j = 0; j < l.Count; j++)
+				{
+					if (i != j)
+					{
+						int dx = l[j].x - l[i].x;
+						int dy = l[j].y - l[i].y;
+
+						int ax = l[i].x - dx;
+						int ay = l[i].y - dy;
+
+						if (ax >= 0 && ax < width && ay >= 0 && ay < height)
+						{
+							locations.Add((ax, ay));
+						}
+					}
+				}
+			}
+		}
+
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				Console.Write(locations.Contains((x, y)) ? 'X' : lines[y][x]);
+			}
+			Console.WriteLine();
+		}
+
+		Console.WriteLine(locations.Count);
+	}
+}
