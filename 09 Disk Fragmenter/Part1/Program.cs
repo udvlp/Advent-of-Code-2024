8bd71d3d104b8c3ce5feb5770468ed1d6df16c9e
Ulf Haueisen <ulf@dvlp.de>
2024-12-11 20:36:49 +0100

    Day 9: Disk Fragmenter

---
+++ 09 Disk Fragmenter/Part1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace AoC;
+
+internal class Program
+{
+	static void Main()
+	{
+		long result = 0;
+		List<int> list = [];
+
+		var sr = new StreamReader(@"..\..\input.txt");
+		var line = sr.ReadLine();
+
+		int k = 0;
+		for (int i = 0; i < line.Length; i++)
+		{
+			int n = line[i] - '0';
+			if (i % 2 == 0)
+			{
+				for (int j = 0; j < n; j++) list.Add(k);
+				k++;
+			}
+			else
+			{
+				for (int j = 0; j < n; j++) list.Add(-1);
+			}
+		}
+
+		int p = 0;
+		int q = list.Count - 1;
+		while (true)
+		{
+			bool ok = false;
+			for (int j = p; j < q; j++)
+			{
+				if (list[j] == -1)
+				{
+					p = j;
+					ok = true;
+					break;
+				}
+			}
+			if (!ok) break;
+			for (int j = q; j >= p; j--)
+			{
+				if (list[j] != -1)
+				{
+					q = j;
+					ok = true;
+					break;
+				}
+			}
+			if (!ok) break;
+			list[p] = list[q];
+			list[q] = -1;
+		}
+
+		for (int i = 0; i < list.Count; i++)
+		{
+			if (list[i] >= 0)
+			{
+				result += (long)i * list[i];
+			}
+		}
+
+		Console.WriteLine(result);
+	}
+}
---
+++ 09 Disk Fragmenter/Part2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace AoC;
+
+internal class Program
+{
+	static void Main()
+	{
+		long result = 0;
+		List<(long pos, long len)> file = [];
+		List<(long pos, long len)> space = [];
+
+		var sr = new StreamReader(@"..\..\input.txt");
+		var line = sr.ReadLine();
+
+		int p = 0;
+		for (int i = 0; i < line.Length; i++)
+		{
+			int l = line[i] - '0';
+			if (i % 2 == 0)
+			{
+				file.Add((p, l));
+			}
+			else
+			{
+				space.Add((p, l));
+			}
+			p += l;
+		}
+
+		for (int i = file.Count - 1; i >= 0; i--)
+		{
+			var f = file[i];
+			for (int j = 0; j < space.Count; j++)
+			{
+				var s = space[j];
+				if (s.pos < f.pos && s.len >= f.len)
+				{
+					file[i] = (s.pos, f.len);
+					space[j] = (s.pos + f.len, s.len - f.len);
+					space.Add((f.pos, f.len));
+					break;
+				}
+			}
+		}
+
+		int k = 0;
+		foreach (var f in file)
+		{
+			result += k * f.len * (2 * f.pos + f.len - 1) / 2L;
+			k++;
+		}
+
+		Console.WriteLine(result);
+	}
+}
